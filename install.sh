db0310d83f9749e35eaf44bd24830ff13f09d316
王九弦SZ·Ninty <120767492+SinzMise@users.noreply.github.com>
2023-08-14 10:26:15 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ echo "GitHub开源地址：https://github.com/SinzMise/memos-on-replit"
 nix-env -iA nixpkgs.wget
 mkdir build
 cd build
-wget -O .replit https://github.com/SinzMise/memos-on-replit/raw/master/.replit
-wget -O replit.nix https://github.com/SinzMise/memos-on-replit/raw/master/replit.nix
+wget -O .replit https://github.com/SinzMise/Artalk-on-replit/raw/master/.replit
+wget -O replit.nix https://github.com/SinzMise/Artalk-on-replit/raw/master/replit.nix
 cd ..
-wget -O main.sh https://github.com/SinzMise/memos-on-replit/raw/master/main.sh
-wget -O memos https://github.com/SinzMise/memos-on-replit/raw/master/artalk.moe
+wget -O main.sh https://github.com/SinzMise/Artalk-on-replit/raw/master/main.sh
+wget -O artalk https://github.com/SinzMise/Artalk-on-replit/raw/master/artalk.moe
 cp -r build/.replit . && cp -r build/replit.nix .
 echo "恭喜搭建完成"
 echo "点击Run立即运行项目"
