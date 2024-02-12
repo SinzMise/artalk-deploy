136db791a8a83f2ba2f508b37e71b8fb23d0a9ce
SinzMise <email@sinzmise.top>
2024-02-12 14:05:17 +0800

    添加sh说明

--- artalk-freebsd.sh
+++ artalk-freebsd.sh
@@ -8,4 +8,19 @@ curl -L $DOWNLOAD_URL -o artalk
 
 chmod +x artalk
 
-echo "Artalk-FreeBSD最新版本已经下载覆盖完成！"
+if [ -f "./artalk.yml" ]; then
+    echo "Artalk-FreeBSD最新版本已经下载覆盖完成！"
+else
+    nohup ./artalk server > /dev/null 2>&1 &
+    clear
+    echo "已生成配置文件，请修改端口！"
+    echo
+    echo "使用命令 vim artalk.yml 进入data路径下"
+    echo
+    echo "将 port: 23366中的 23366 修改成你放行的端口即可"
+    echo
+    echo "接着按 Esc 键退出插入模式，再按 : 键进入命令模式，并输入 wq 再回车，保存并退出"
+    echo
+    echo "再使用命令 cd .. 回到上级目录"
+    echo
+fi
