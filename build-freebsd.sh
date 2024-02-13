6088a3febacfb9d7b7387d0cbbacf742894d07de
王九弦SZ·Ninty <120767492+SinzMise@users.noreply.github.com>
2024-02-13 16:35:54 +0800

    Create build-freebsd.sh

---
+++ build-freebsd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+PKG_NAME="github.com/ArtalkJS/Artalk"
+BIN_NAME="./bin/artalk-freebsd.moe"
+VERSION=$(git describe --tags --abbrev=0)
+COMMIT_HASH=$(git rev-parse --short HEAD)
+
+go mod tidy
+go build \
+    	-ldflags "-s -w -X $PKG_NAME/internal/config.Version=$VERSION \
+        -X $PKG_NAME/internal/config.CommitHash=$COMMIT_HASH" \
+        -o $BIN_NAME \
+    	$PKG_NAME
