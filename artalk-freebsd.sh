b5af1c15cbef87c45f584a12a2d9531fee37a681
王九弦SZ·Ninty <120767492+SinzMise@users.noreply.github.com>
2024-04-14 15:59:09 +0800

    Update artalk-freebsd.sh

--- artalk-freebsd.sh
+++ artalk-freebsd.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 
 API_URL="https://api.github.com/repos/SinzMise/artalk-deploy/releases/latest"
+DOWNLOAD_URL=$(curl -s $API_URL | jq -r ".assets[] | select(.name == \"Artalk-freebsd-amd64.tar.gz\") | .browser_download_url")
+curl -L $DOWNLOAD_URL -o artalk-freebsd-amd64.tar.gz
+tar -xzvf artalk-freebsd-amd64.tar.gz && rm artalk-freebsd-amd64.tar.gz && chmod +x artalk
 
-DOWNLOAD_URL=$(curl -s $API_URL | jq -r ".assets[] | select(.name | contains(\"artalk-freebsd.moe\")) | .browser_download_url")
-
-curl -L $DOWNLOAD_URL -o artalk
-
-chmod +x artalk
-
-if [ -f "./artalk.yml" ]; then
+if [ -f "./data/ip2region.xdb" ]; then
     echo "Artalk-FreeBSD最新版本已经下载覆盖完成！"
 else
+    curl -L https://github.com/lionsoul2014/ip2region/raw/master/data/ip2region.xdb -o ./data/ip2region.xdb
     nohup ./artalk server > /dev/null 2>&1 &
     clear
-    curl -L https://github.com/lionsoul2014/ip2region/raw/master/data/ip2region.xdb -o ./data/ip2region.xdb
     echo "已生成配置文件，请修改端口！"
     echo
     echo "使用命令 vim artalk.yml 修改artalk.yml文件"
